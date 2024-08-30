1ccb28b5317e52804a70ebe68a8761da64a120dc
Norbert Hańderek <norbert.handerek03@gmail.com>
2024-08-30 23:35:59 +0200

    Lua override + WezTerm leader size adjust

--- shell/.wezterm.lua
+++ shell/.wezterm.lua
@@ -1,7 +1,7 @@
 local wezterm = require("wezterm")
 
 local config = wezterm.config_builder()
-config.automatically_reload_config = true 
+config.automatically_reload_config = true
 
 config.font = wezterm.font("JetBrainsMono Nerd Font")
 config.font_size = 16
@@ -18,85 +18,85 @@ config.macos_window_background_blur = 10
 -- Multiplexer
 config.leader = { key = "q", mods = "ALT", timeout_milliseconds = 2000 }
 config.keys = {
-    {
-        mods = "LEADER",
-        key = "c",
-        action = wezterm.action.SpawnTab "CurrentPaneDomain",
-    },
-    {
-        mods = "LEADER",
-        key = "x",
-        action = wezterm.action.CloseCurrentPane { confirm = true }
-    },
-    {
-        mods = "LEADER",
-        key = "|",
-        action = wezterm.action.SplitHorizontal { domain = "CurrentPaneDomain" }
-    },
-    {
-        mods = "LEADER",
-        key = "-",
-        action = wezterm.action.SplitVertical { domain = "CurrentPaneDomain" }
-    },
-    {
-        mods = "CTRL",
-        key = "b",
-        action = wezterm.action.ActivateTabRelative(-1)
-    },
-    {
-        mods = "CTRL",
-        key = "n",
-        action = wezterm.action.ActivateTabRelative(1)
-    },
-    {
-        mods = "CTRL",
-        key = "h",
-        action = wezterm.action.ActivatePaneDirection "Left"
-    },
-    {
-        mods = "CTRL",
-        key = "j",
-        action = wezterm.action.ActivatePaneDirection "Down"
-    },
-    {
-        mods = "CTRL",
-        key = "k",
-        action = wezterm.action.ActivatePaneDirection "Up"
-    },
-    {
-        mods = "CTRL",
-        key = "l",
-        action = wezterm.action.ActivatePaneDirection "Right"
-    },
-    {
-        mods = "CTRL",
-        key = "LeftArrow",
-        action = wezterm.action.AdjustPaneSize { "Left", 5 }
-    },
-    {
-        mods = "CTRL",
-        key = "RightArrow",
-        action = wezterm.action.AdjustPaneSize { "Right", 5 }
-    },
-    {
-        mods = "CTRL",
-        key = "DownArrow",
-        action = wezterm.action.AdjustPaneSize { "Down", 5 }
-    },
-    {
-        mods = "CTRL",
-        key = "UpArrow",
-        action = wezterm.action.AdjustPaneSize { "Up", 5 }
-    },
+	{
+		mods = "LEADER",
+		key = "c",
+		action = wezterm.action.SpawnTab("CurrentPaneDomain"),
+	},
+	{
+		mods = "LEADER",
+		key = "x",
+		action = wezterm.action.CloseCurrentPane({ confirm = true }),
+	},
+	{
+		mods = "LEADER",
+		key = "|",
+		action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }),
+	},
+	{
+		mods = "LEADER",
+		key = "-",
+		action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }),
+	},
+	{
+		mods = "CTRL",
+		key = "b",
+		action = wezterm.action.ActivateTabRelative(-1),
+	},
+	{
+		mods = "CTRL",
+		key = "n",
+		action = wezterm.action.ActivateTabRelative(1),
+	},
+	{
+		mods = "CTRL",
+		key = "h",
+		action = wezterm.action.ActivatePaneDirection("Left"),
+	},
+	{
+		mods = "CTRL",
+		key = "j",
+		action = wezterm.action.ActivatePaneDirection("Down"),
+	},
+	{
+		mods = "CTRL",
+		key = "k",
+		action = wezterm.action.ActivatePaneDirection("Up"),
+	},
+	{
+		mods = "CTRL",
+		key = "l",
+		action = wezterm.action.ActivatePaneDirection("Right"),
+	},
+	{
+		mods = "LEADER",
+		key = "LeftArrow",
+		action = wezterm.action.AdjustPaneSize({ "Left", 10 }),
+	},
+	{
+		mods = "LEADER",
+		key = "RightArrow",
+		action = wezterm.action.AdjustPaneSize({ "Right", 10 }),
+	},
+	{
+		mods = "LEADER",
+		key = "DownArrow",
+		action = wezterm.action.AdjustPaneSize({ "Down", 10 }),
+	},
+	{
+		mods = "LEADER",
+		key = "UpArrow",
+		action = wezterm.action.AdjustPaneSize({ "Up", 10 }),
+	},
 }
 
 for i = 0, 9 do
-    -- leader + number to activate that tab
-    table.insert(config.keys, {
-        key = tostring(i),
-        mods = "LEADER",
-        action = wezterm.action.ActivateTab(i),
-    })
+	-- leader + number to activate that tab
+	table.insert(config.keys, {
+		key = tostring(i),
+		mods = "LEADER",
+		action = wezterm.action.ActivateTab(i),
+	})
 end
 
 -- Tab bar
@@ -107,89 +107,89 @@ config.tab_and_split_indices_are_zero_based = true
 
 -- Status
 wezterm.on("update-right-status", function(window, _)
-    local SOLID_LEFT_ARROW = ""
-    local ARROW_FOREGROUND = { Foreground = { Color = "#c6a0f6" } }
-    local prefix = ""
+	local SOLID_LEFT_ARROW = ""
+	local ARROW_FOREGROUND = { Foreground = { Color = "#c6a0f6" } }
+	local prefix = ""
 
-    if window:leader_is_active() then
-        prefix = " " .. utf8.char(0x26A1) -- ocean wave
-        SOLID_LEFT_ARROW = utf8.char(0xe0b2)
-    end
+	if window:leader_is_active() then
+		prefix = " " .. utf8.char(0x26A1) -- ocean wave
+		SOLID_LEFT_ARROW = utf8.char(0xe0b2)
+	end
 
-    if window:active_tab():tab_id() ~= 0 then
-        ARROW_FOREGROUND = { Foreground = { Color = "#1e2030" } }
-    end -- arrow color based on if tab is first pane
+	if window:active_tab():tab_id() ~= 0 then
+		ARROW_FOREGROUND = { Foreground = { Color = "#1e2030" } }
+	end -- arrow color based on if tab is first pane
 
-    window:set_left_status(wezterm.format {
-        { Text = prefix },
-        ARROW_FOREGROUND,
-        { Text = SOLID_LEFT_ARROW }
-    })
+	window:set_left_status(wezterm.format({
+		{ Text = prefix },
+		ARROW_FOREGROUND,
+		{ Text = SOLID_LEFT_ARROW },
+	}))
 end)
 
 config.colors = {
-  foreground = "#c6cad7",
-  background = "#12141a",
-  cursor_bg = "#ffd866",
-  cursor_border = "#ffd866",
-  cursor_fg = "#12141a",
-  selection_bg = "#98a2b5",
-  selection_fg = "#12141a",
+	foreground = "#c6cad7",
+	background = "#12141a",
+	cursor_bg = "#ffd866",
+	cursor_border = "#ffd866",
+	cursor_fg = "#12141a",
+	selection_bg = "#98a2b5",
+	selection_fg = "#12141a",
 
-  ansi = {
-    "#12141a",
-    "#fc6a67",
-    "#a9dc76",
-    "#ffd866",
-    "#78dce8",
-    "#e991e3",
-    "#78e8c6",
-    "#12141a",
-  },
-  
-  brights = {
-    "#404658",
-    "#ff6764",
-    "#a9f65c",
-    "#ffd866",
-    "#61eeff",
-    "#fd7df4",
-    "#61ffcf",
-    "#404658",
-  },
+	ansi = {
+		"#12141a",
+		"#fc6a67",
+		"#a9dc76",
+		"#ffd866",
+		"#78dce8",
+		"#e991e3",
+		"#78e8c6",
+		"#12141a",
+	},
 
-  tab_bar = {
-    background = "#171921",
-    active_tab = {
-      bg_color = "#12141a",
-      fg_color = "#98a2b5",
-      underline = "None",
-    },
-    inactive_tab = {
-      bg_color = "#171921",
-      fg_color = "#4c526b",
-    },
-    inactive_tab_hover = {
-      bg_color = "#12141a",
-      fg_color = "#98a2b5",
-    },
-    new_tab = {
-      bg_color = "#171921",
-      fg_color = "#98a2b5",
-    },
-    new_tab_hover = {
-      bg_color = "#12141a",
-      fg_color = "#ffd866",
-    },
-  },
+	brights = {
+		"#404658",
+		"#ff6764",
+		"#a9f65c",
+		"#ffd866",
+		"#61eeff",
+		"#fd7df4",
+		"#61ffcf",
+		"#404658",
+	},
 
-  scrollbar_thumb = "#c6cad7",
+	tab_bar = {
+		background = "#171921",
+		active_tab = {
+			bg_color = "#12141a",
+			fg_color = "#98a2b5",
+			underline = "None",
+		},
+		inactive_tab = {
+			bg_color = "#171921",
+			fg_color = "#4c526b",
+		},
+		inactive_tab_hover = {
+			bg_color = "#12141a",
+			fg_color = "#98a2b5",
+		},
+		new_tab = {
+			bg_color = "#171921",
+			fg_color = "#98a2b5",
+		},
+		new_tab_hover = {
+			bg_color = "#12141a",
+			fg_color = "#ffd866",
+		},
+	},
 
-  split = "#0a0b0f",
+	scrollbar_thumb = "#c6cad7",
 
-  visual_bell = "#fc6a67",
+	split = "#0a0b0f",
 
-  compose_cursor = "#fc9867",
+	visual_bell = "#fc6a67",
+
+	compose_cursor = "#fc9867",
 }
 
 return config
