2c18238c80374229a252faff14871eec25381efd
Norbert Hańderek <norbert.handerek03@gmail.com>
2025-01-04 23:05:26 +0100

    feat: ghostty! 👻

--- link.sh
+++ link.sh
@@ -17,7 +17,7 @@ fi
 SCRIPT=$(readlink -f $0)
 SCRIPT_DIR=`dirname $SCRIPT`
 
-remove_existing () {
+remove_existing() {
   # If it's a symlink - remove
   if [ -L "$1" ]; then
     rm "$1"
@@ -28,20 +28,26 @@ remove_existing () {
   fi
 }
 
-# shell
-remove_existing ~/.tmux.conf
-ln -s "$SCRIPT_DIR/shell/.tmux.conf" ~/.tmux.conf
-remove_existing ~/.wezterm.lua
-ln -s "$SCRIPT_DIR/shell/.wezterm.lua" ~/.wezterm.lua
+create_symlink() {
+    local target="$1"
+    local link="$2"
+    mkdir -p "$(dirname "$link")" && ln -s "$target" "$link"
+}
+
+# Terminal
+remove_existing ~/.config/ghostty
+create_symlink "$SCRIPT_DIR/ghostty" ~/.config/ # Automatically adds 'ghostty' as it's a directory
+
+# Shell
 remove_existing ~/.zshrc
-ln -s "$SCRIPT_DIR/shell/.zshrc" ~/.zshrc
+create_symlink "$SCRIPT_DIR/shell/.zshrc" ~/.zshrc
 
 # editors
 remove_existing ~/.config/nvim
-ln -s "$SCRIPT_DIR/nvim" ~/.config/ # Automatically adds 'nvim'
+create_symlink "$SCRIPT_DIR/nvim" ~/.config/ # Automatically adds 'nvim' as it's a directory
 remove_existing "$VS_CODE_CONFIG/settings.json"
-ln -s "$SCRIPT_DIR/vscode/settings.json" "$VS_CODE_CONFIG/settings.json"
+create_symlink "$SCRIPT_DIR/vscode/settings.json" "$VS_CODE_CONFIG/settings.json"
 remove_existing "$VS_CODE_CONFIG/keybindings.json"
-ln -s "$SCRIPT_DIR/vscode/keybindings.json" "$VS_CODE_CONFIG/keybindings.json"
+create_symlink "$SCRIPT_DIR/vscode/keybindings.json" "$VS_CODE_CONFIG/keybindings.json"
 
 echo "Done"
