b655078e3957e3ab5724e46d4dff2bc771d9896f
Norbert Hańderek <norbert.handerek03@gmail.com>
2025-06-09 19:48:08 +0200

    nvim: extend gitsigns mappings

--- nvim/lua/plugins/gitsigns.lua
+++ nvim/lua/plugins/gitsigns.lua
@@ -13,16 +13,45 @@ local function config()
 
   local keymap_opts = { remap = true, silent = true }
 
-  -- Map ]g + [c to navigate through hunks
-  vim.keymap.set("n", "]c", gitsigns.next_hunk, keymap_opts)
-  vim.keymap.set("n", "[c", gitsigns.prev_hunk, keymap_opts)
+  -- git navigation
+  vim.keymap.set("n", "]c", function()
+    if vim.wo.diff then
+      vim.cmd.normal({ "]c", bang = true })
+    else
+      gitsigns.nav_hunk("next")
+    end
+  end, keymap_opts)
 
-  -- Map git blame, preview, reset, stage, and undo stage
+  vim.keymap.set("n", "[c", function()
+    if vim.wo.diff then
+      vim.cmd.normal({ "[c", bang = true })
+    else
+      gitsigns.nav_hunk("prev")
+    end
+  end, keymap_opts)
+
+  vim.keymap.set({'o', 'x'}, 'ih', gitsigns.select_hunk, keymap_opts)
+
+  -- git actions
   vim.keymap.set("n", "<leader>bl", gitsigns.blame_line, keymap_opts)
   vim.keymap.set("n", "<leader>hp", gitsigns.preview_hunk, keymap_opts)
-  vim.keymap.set("n", "<leader>hr", gitsigns.reset_hunk, keymap_opts)
-  vim.keymap.set("n", "<leader>hs", gitsigns.stage_hunk, keymap_opts)
-  vim.keymap.set("n", "<leader>hus", gitsigns.undo_stage_hunk, keymap_opts)
+
+  -- -- gitsigns.stage actually means "git add"
+  vim.keymap.set("n", "<leader>ha", gitsigns.stage_hunk, keymap_opts)
+  vim.keymap.set("v", "<leader>ha", function()
+    gitsigns.stage_hunk({ vim.fn.line("."), vim.fn.line("v") })
+  end, keymap_opts)
+  vim.keymap.set("n", "<leader>ba", gitsigns.stage_buffer, keymap_opts)
+
+  -- -- gitsigns.undo_stage actually means "git reset", it unstages changes
+  vim.keymap.set("n", "<leader>hr", gitsigns.undo_stage_hunk, keymap_opts)
+
+  -- -- gitsigns.reset actually means "git checkout", it reverts unstaged changes
+  vim.keymap.set("n", "<leader>hc", gitsigns.reset_hunk, keymap_opts)
+  vim.keymap.set("v", "<leader>hc", function()
+    gitsigns.reset_hunk({ vim.fn.line("."), vim.fn.line("v") })
+  end, keymap_opts)
+  vim.keymap.set("n", "<leader>bc", gitsigns.reset_buffer, keymap_opts)
 end
 
 return {
