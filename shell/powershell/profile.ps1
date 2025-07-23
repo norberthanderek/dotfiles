3d5597f6ced905809be371c8909ba3470e29e9cb
Norbert Hańderek <norbert.handerek03@gmail.com>
2025-07-23 15:37:09 +0200

    powershell: open new tab in pwd

--- shell/powershell/profile.ps1
+++ shell/powershell/profile.ps1
@@ -5,3 +5,14 @@ $extensionsPath = Join-Path $dotfiles 'shell\powershell\extensions'
 Get-ChildItem -Path $extensionsPath -Filter '*.ps1' | ForEach-Object {
     . $_.FullName
 }
+
+# Open next tab in the same directory
+function prompt {
+    $loc = $executionContext.SessionState.Path.CurrentLocation;
+    $out = ""
+    if ($loc.Provider.Name -eq "FileSystem") {
+        $out += "$([char]27)]9;9;`"$($loc.ProviderPath)`"$([char]27)\"
+    }
+    $out += "PS $loc$('>' * ($nestedPromptLevel + 1)) ";
+    return $out
+}
