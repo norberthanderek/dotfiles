5a7ed8431c4b114fed877cb427a3f9133b237602
Norbert Hańderek <norbert.handerek03@gmail.com>
2025-08-27 14:26:06 +0200

    nvim: windows terminal comment shortcut fix

--- nvim/lua/config/mappings.lua
+++ nvim/lua/config/mappings.lua
@@ -1,5 +1,11 @@
 local remap_opts = { remap = true, silent = true }
 
 -- Remap commenting
-vim.keymap.set("n", "<C-/>", "gcc", remap_opts)
-vim.keymap.set("v", "<C-/>", "gc", remap_opts)
+if require("utils.os").is_windows() then
+  -- <C-_> means <C-/> on Windows Terminal ¯\_(ツ)_/¯
+  vim.keymap.set("n", "<C-_>", "gcc", remap_opts)
+  vim.keymap.set("v", "<C-_>", "gc", remap_opts)
+else
+  vim.keymap.set("n", "<C-/>", "gcc", remap_opts)
+  vim.keymap.set("v", "<C-/>", "gc", remap_opts)
+end
